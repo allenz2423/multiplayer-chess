0cd34832cab436ba858f434c70fdfddda533af3b
Allen Zhao <allenz0509@gmail.com>
2024-07-24 18:44:50 -0400

    added some basic code and fixed some other stuff

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,40 +1,62 @@
 use std::collections::HashMap;
 #[derive(Clone)]
 enum Pieces {
-    King,
-    Queen,
-    Knight,
-    Bishop,
-    Rook,
-    Pawn,
-    Empty,
+    King(King),
+    Queen(Queen),
+    Knight(Knight),
+    Bishop(Bishop),
+    Rook(Rook),
+    Pawn(Pawn),
 }
 
-type PossibleMoves = HashMap<i8, Vec<(i8,i8)>>;
-
+type PossibleMoves = HashMap<i8, Vec<i8>>;
+#[derive(Clone)]
 pub struct Board {
-    board:Vec<Vec<Pieces>>,
+    board:Vec<Vec<Option<Pieces>>>,
     gameStatus:String,
 }
 impl Board {
     pub fn new() -> Self {
+        let mut board:Vec<Vec<Option<Pieces>>>;
+        board = vec![vec![None;8];8];
+        for i in 0..7 {
+            board[i][1] = Some(Pieces::Pawn(Pawn::new("White".to_string(),vec![i as i8,1 as i8])));
+            board[i][6] = Some(Pieces::Pawn(Pawn::new("Black".to_string(),vec![i as i8,1 as i8])));
+        };
+        board[0][0] = Some(Pieces::Rook(Rook::new("White".to_string(), vec![0 as i8, 0 as i8])));
+        board[7][0] = Some(Pieces::Rook(Rook::new("White".to_string(), vec![7 as i8, 0 as i8])));
+        board[0][7] = Some(Pieces::Rook(Rook::new("Black".to_string(), vec![0 as i8, 7 as i8])));
+        board[7][7] = Some(Pieces::Rook(Rook::new("Black".to_string(), vec![7 as i8, 7 as i8])));
+        board[1][0] = Some(Pieces::Knight(Knight::new("White".to_string(), vec![1 as i8, 0 as i8])));
+        board[6][0] = Some(Pieces::Knight(Knight::new("White".to_string(), vec![6 as i8, 0 as i8])));
+        board[1][7] = Some(Pieces::Knight(Knight::new("Black".to_string(), vec![1 as i8, 7 as i8])));
+        board[6][7] = Some(Pieces::Knight(Knight::new("Black".to_string(), vec![6 as i8, 7 as i8])));
+        board[2][0] = Some(Pieces::Bishop(Bishop::new("White".to_string(), vec![2 as i8, 0 as i8])));
+        board[5][0] = Some(Pieces::Bishop(Bishop::new("White".to_string(), vec![5 as i8, 0 as i8])));
+        board[2][7] = Some(Pieces::Bishop(Bishop::new("Black".to_string(), vec![2 as i8, 7 as i8])));
+        board[5][7] = Some(Pieces::Bishop(Bishop::new("Black".to_string(), vec![5 as i8, 7 as i8])));
+        board[3][0] = Some(Pieces::Queen(Queen::new("White".to_string(), vec![3 as i8, 0 as i8])));
+        board[4][0] = Some(Pieces::King(King::new("White".to_string(), vec![4 as i8, 0 as i8])));
+        board[3][7] = Some(Pieces::Queen(Queen::new("Black".to_string(), vec![3 as i8, 7 as i8])));
+        board[4][7] = Some(Pieces::King(King::new("Black".to_string(), vec![4 as i8, 7 as i8])));
         Self {
-            board:vec![vec![Pieces::Empty;8];8],
+            board,
             gameStatus:"Ongoing".to_string(),
         }
     }
 }
-
+#[derive(Clone)]
 pub struct King {
     position:Vec<i8>,
     inCheck:bool,
     color:String,
     name:String,
-    possibleMoves:HashMap<i8, Vec<(i8, i8)>>,
-    board:Board,
+    possibleMoves:HashMap<i8, Vec<i8>>,
+    ownBoard:Board,
+    
 }
 impl King {
-    pub fn get_moves() {
+    pub fn get_moves() -> HashMap<i8, Vec<i8>> {
         let mut possibleMoves:PossibleMoves = HashMap::new();
         let mut tracker:i8 = 0;
         for i in -1..2 {
@@ -43,10 +65,10 @@ impl King {
                     continue
                 }
                 tracker+=1;
-                possibleMoves.insert(tracker, vec![(i,j)]);
+                possibleMoves.insert(tracker, vec![i,j]);
             }
         }
-        possibleMoves;
+        return possibleMoves;
     }
     pub fn is_valid_move(&self, otherKing:King) -> bool {
         // You have to check if you are within 2 units of distance of the other King
@@ -57,12 +79,14 @@ impl King {
             return false
         }
         for i in 0..8 {
-            
+           for j in 0..8 {
+               // if board.
+           } 
         }
         true
 
     }
-    pub fn new(&self,color:String) -> Self {
+    pub fn new(color:String, pos:Vec<i8>) -> Self {
         let mut position;
         let position = if color == "White" {
                 position = vec![0 as i8,4 as i8]
@@ -70,27 +94,123 @@ impl King {
                 position = vec![7 as i8,4 as i8]
             };
         Self {
-            position: self.position.clone(),
+            position: pos,
             inCheck:false,
             color,
             name:"King".to_string(),
-            possibleMoves: self.possibleMoves.clone(),
-            board: Board::new(),
+            possibleMoves:Self::get_moves(),
+            ownBoard: Board::new(),
         }
     }
 }
+#[derive(Clone)]
 pub struct Queen {
-
+    position:Vec<i8>,
+    color:String,
+    name:String,
+    possibleMoves:HashMap<i8, Vec<i8>>,
+    
 }
+impl Queen {
+    pub fn new(color:String, pos:Vec<i8>) -> Self {
+        let mut possibleMoves:HashMap<i8,Vec<i8>> = HashMap::new();
+        possibleMoves.insert(1,vec![pos[0],pos[1]]);
+
+        Self {
+            position:pos,
+            color,
+            name: "Queen".to_string(),
+            possibleMoves,
+            
+        }
+    }
+}
+#[derive(Clone)]
 pub struct Knight {
-
+    position:Vec<i8>,
+    color:String,
+    name:String,
+    possibleMoves:HashMap<i8, Vec<i8>>,
+    
 }
+impl Knight {
+    pub fn new(color:String, pos:Vec<i8>) -> Self {
+        let mut possibleMoves:HashMap<i8,Vec<i8>> = HashMap::new();
+        possibleMoves.insert(1,vec![pos[0],pos[1]]);
+        Self {
+            position:pos,
+            color,
+            name: "Knight".to_string(),
+            
+            possibleMoves,
+            
+        }
+    }
+}
+#[derive(Clone)]
 pub struct Bishop {
-
+    position:Vec<i8>,
+    color:String,
+    name:String,
+    possibleMoves:HashMap<i8, Vec<i8>>,
+    
 }
+impl Bishop {
+    pub fn new(color:String, pos:Vec<i8>) -> Self {
+        let mut possibleMoves:HashMap<i8,Vec<i8>> = HashMap::new();
+        possibleMoves.insert(1,vec![pos[0],pos[1]]);
+        Self {
+            position:pos,
+            color,
+            name: "Bishop".to_string(),
+            
+            possibleMoves,
+            
+        }
+    }
+}
+#[derive(Clone)]
 pub struct Rook {
-
+    position:Vec<i8>,
+    color:String,
+    name:String,
+    possibleMoves:HashMap<i8, Vec<i8>>,
+    
 }
+impl Rook {
+    pub fn new(color:String, pos:Vec<i8>) -> Self {
+        let mut possibleMoves:HashMap<i8,Vec<i8>> = HashMap::new();
+        possibleMoves.insert(1,vec![pos[0],pos[1]]);
+        Self {
+            position:pos,
+            color,
+            name: "Rook".to_string(),
+            
+            possibleMoves,
+            
+        }
+    }
+}
+#[derive(Clone)]
 pub struct Pawn {
-
+    position:Vec<i8>,
+    color:String,
+    name:String,
+    possibleMoves:HashMap<i8, Vec<i8>>,
+    
+    enpassantPossible:bool,
+    
+}
+impl Pawn {
+    pub fn new(color:String, pos:Vec<i8>) -> Self {
+        let mut possibleMoves:HashMap<i8,Vec<i8>> = HashMap::new();
+        possibleMoves.insert(1,vec![pos[0],pos[1]]);
+        Self {
+            position:pos,
+            color,
+            name: "Pawn".to_string(),
+           possibleMoves, 
+            enpassantPossible:true,
+        }
+    }
 }
